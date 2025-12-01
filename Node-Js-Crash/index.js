7a6d90e0983c609098f186db01092b517a0cdee7
nour <noure8317@gmail.com>
2025-12-01 22:44:53 +0200

    few adjustments

---
+++ Node-Js-Crash/index.js
@@ -0,0 +1,14 @@
+import { getPosts } from "./PostController.js";
+
+console.log(getPosts())
+
+
+
+
+
+// const {genereateRandomNumber, printTheKingName} = require('./utiles');
+
+// console.log(`Random Floor Number: ${genereateRandomNumber()}`);
+// console.log(printTheKingName());
+
+
